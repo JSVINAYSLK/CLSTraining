a3c59ad8615a3918f0f5b7bf036d2ba47c44134d
JSVINAYSLK <j.vinay@slkgroup.com>
2025-03-12 19:57:58 +0530

    Completed Class views,files,sessions in Django

--- Django/feedback/feedback/settings.py
+++ Django/feedback/feedback/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "reviews",
+    "profiles",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -118,6 +119,8 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+MEDIA_ROOT = BASE_DIR / "uploads"
+MEDIA_URL = "/user-media/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
