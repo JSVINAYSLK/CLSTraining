9e5802f91ab38dffa832fdc398959965abc5e43b
JSVINAYSLK <j.vinay@slkgroup.com>
2025-04-03 18:21:15 +0530

    Completed loops,functions and conditionals in Javascript

--- Javascript/javascript-in-a-hurry-initial-main/main.js
+++ Javascript/javascript-in-a-hurry-initial-main/main.js
@@ -25,12 +25,66 @@ document.querySelector("#close-nav-menu").addEventListener("click", function(){
 
 // // console.log("After adding , your new balance is "+balance);
 // header section
+function celsiusToFahr(temperature){
+    let fahr = (temperature * 9/5) + 32;
+    return fahr;
+}
+
 const greetingText="Good Morning!"
 const weatherCondition="Sunny"
 const userLocation="Chittoor"
-let temperature=39.7
-let weatherText=`The weather is ${weatherCondition} in ${userLocation} and it's ${temperature.toString()} outside.`
+let temperature=33.7
+
+let celsiusText=`The weather is ${weatherCondition} in ${userLocation} and it's ${temperature.toFixed(1)}C outside.`
+let fahrText=`The weather is ${weatherCondition} in ${userLocation} and it's ${celsiusToFahr(temperature)}F outside.`
 
 document.querySelector("#greeting").innerHTML=greetingText
+document.querySelector("p#weather").innerHTML=celsiusText
+document.querySelector(".weather-group").addEventListener("click",function(e){
+    if(e.target.id=="celsius"){
+        document.querySelector("#weather").innerHTML=celsiusText;
+    }else if(e.target.id=="fahr"){
+        document.querySelector("#weather").innerHTML=fahrText;
+    }
+});
+// let localTime = new Date();
+// document.querySelector("span[data-time=hours]").textContent=localTime.getHours();
+// document.querySelector("span[data-time=minutes]").textContent=localTime.getMinutes();
+// document.querySelector("span[data-time=seconds]").textContent=localTime.getSeconds();
+setInterval(function() {
+    localTime = new Date();
+    document.querySelector("span[data-time=hours]").textContent=localTime.getHours().toString().padStart(2, '0');
+    document.querySelector("span[data-time=minutes]").textContent=localTime.getMinutes().toString().padStart(2, '0');
+    document.querySelector("span[data-time=seconds]").textContent=localTime.getSeconds().toString().padStart(2, '0');
+}, 1000);
 
-document.querySelector("p#weather").innerHTML=weatherText
+const galleryImages=[
+    {
+        src:"./assets/gallery/image1.jpg",
+        alt:"Thumbnail Image 1"
+    },
+    {
+        src:"./assets/gallery/image2.jpg",
+        alt:"Thumbnail Image 2"
+    },
+    {
+        src:"./assets/gallery/image3.jpg",
+        alt:"Thumbnail Image 3"
+    }
+]
+let mainImage=document.querySelector("#gallery > img");
+let thumbnails=document.querySelector("#gallery .thumbnails");
+mainImage.src=galleryImages[0].src;
+mainImage.alt=galleryImages[0].alt;
+galleryImages.forEach(function(image, index){
+    let thumbnail=document.createElement("img");
+    thumbnail.src=image.src;
+    thumbnail.alt=image.alt;
+    thumbnail.dataset.index=index;
+    thumbnails.appendChild(thumbnail);
+    thumbnail.addEventListener("click", function(){
+        mainImage.src=image.src;
+        mainImage.alt=image.alt;
+    });
+});
+
